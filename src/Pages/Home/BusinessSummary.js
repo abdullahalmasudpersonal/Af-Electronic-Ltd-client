73f6a374a81031c8181c207d5d04a2ea5e77b8f3
masud <abdullahalmasud01737906772@gmail.com>
2022-05-23 22:58:26 +0600

    add summery sections

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import countries from '../../img/Busniness Summary/globe-solid.svg';
+import countries from '../../img/Busniness Summary/icons8-countries-64.png';
+import feedback from '../../img/Busniness Summary/feedback.png';
+import review from '../../img/Busniness Summary/review.png';
+import tools from '../../img/Busniness Summary/tools.png';
+import './BusinessSummary.css';
 
 const BusinessSummary = () => {
     return (
-        <div>
-            <h3 className='text-3xl text-center m-5 font-bold'>Business Summary</h3 >
-          <div>
-              <img src={countries} width='40px' alt='' />
-              <h4 className='text-2xl'>99</h4>
-              <p className=''>Countries</p>
-          </div>
+        <div className='summary-bg'>
+          <h3 className='text-4xl text-center font-bold  mb-10'>Business Summary</h3 >
+
+            <div className='container-card'>
+
+                <div className='business-item'>
+                    <div>
+                        <img src={countries} width='60px' alt='' />
+                        <h4 className='text-2xl text-center font-bold'>99</h4>
+                        <p className='text-center text-1xl font-bold'>Countries</p>
+                    </div>
+                </div>
+                <div className='business-item'>
+                    <div>
+                        <img src={tools} width='60px' alt='' />
+                        <h4 className='text-2xl text-center font-bold'>200+</h4>
+                        <p className='text-center text-1xl font-bold'>Tools</p>
+                    </div>
+                </div>
+                <div className='business-item'>
+                    <div>
+                        <img src={feedback} width='60px' alt='' />
+                        <h4 className='text-2xl text-center font-bold'>10K+</h4>
+                        <p className='text-center text-1xl font-bold'>Feedback</p>
+                    </div>
+                </div>
+                <div className='business-item'>
+                    <div>
+                        <img src={review} width='60px' alt='' />
+                        <h4 className='text-2xl text-center font-bold'>20K+</h4>
+                        <p className='text-center text-1xl font-bold'>Review</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
