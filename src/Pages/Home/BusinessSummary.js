ed8e0a52059bf44742d5eec0834649470239ebf2
masud <abdullahalmasud01737906772@gmail.com>
2022-05-23 11:50:12 +0600

    add env.local

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import countries from '../../img/Busniness Summary/globe-solid.svg';
 
 const BusinessSummary = () => {
     return (
         <div>
-            <h4>Business Summary</h4>
+            <h3 className='text-3xl text-center m-5 font-bold'>Business Summary</h3 >
+          <div>
+              <img src={countries} width='40px' alt='' />
+              <h4 className='text-2xl'>99</h4>
+              <p className=''>Countries</p>
+          </div>
         </div>
     );
 };
